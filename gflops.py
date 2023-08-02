682880f534f8a14e2fb255fb636b0e8368231f7f
chenxiang <christerowo@outlook.com>
2023-08-02 09:34:55 +0800

    huanchuang compute quato and test match

---
+++ gflops.py
@@ -0,0 +1,26 @@
+import torch
+import re
+import os
+from lib.model_test import D2Net
+from ptflops import get_model_complexity_info
+
+os.environ['CUDA_VISIBLE_DEVICES']='1,2' # set device
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda:1" if use_cuda else "cpu")
+# Creating CNN model
+model = D2Net(
+    model_file='models/d2_ots.pth',
+    use_relu='store_false',
+    use_cuda=use_cuda
+)
+
+# Model thats already available
+net = model
+macs, params = get_model_complexity_info(net, (3,480,640), as_strings=True,
+                                         print_per_layer_stat=True, verbose=True)
+# compute gflops
+flops = eval(re.findall(r'([\d.]+)', macs)[0])*2
+flops_unit = re.findall(r'([A-Za-z]+)', macs)[0][0]
+print('Computational complexity: {:<8}'.format(macs))
+print('Computational complexity: {} {}Flops'.format(flops, flops_unit))
+print('Number of parameters: {:<8}'.format(params))
